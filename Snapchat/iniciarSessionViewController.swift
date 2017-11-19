698b79e6702a23a2d3c68db88a06346029670740
Mac Tecsup <gustavo.chavez.c@tecsup.edu.pe>
2017-11-18 19:14:36 -0500

    continua

--- Snapchat/iniciarSessionViewController.swift
+++ Snapchat/iniciarSessionViewController.swift
@@ -23,8 +23,19 @@ class iniciarSessionViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in print("Intentamos iniciar session")
             if error != nil {
                 print("Tenemos el siguiente error: \(error)")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in print("Intentamos crear un usuario")
+                    if error != nil {
+                        print("Tenemosel siguiente error: \(error)")
+                    }else {
+                       print("El usuario fue creado existosamente")
+                        Database.database().reference().child("usuarios").child(user!.uid).child("email").setValue(user!.email)
+                        
+                        self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                    }
+                })
             }else{
                 print("Inicio sesion exitosos")
+                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
             }
         })
     }
